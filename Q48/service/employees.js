9ab11a3aaa45306e3e609f00c3f59312e00589e5
SriganeshIncedo <114339724+SriganeshIncedo@users.noreply.github.com>
2022-12-09 13:28:37 +0530

    Create employees.js

---
+++ Q48/service/employees.js
@@ -0,0 +1,34 @@
+var employees=[        {
+    "EmployeeID": "0",
+    "EmployeeName": "Mullapudi Sriganesh",
+    "Role": "HR",
+    "Salary":"45000",
+    
+},
+{
+    "EmployeeID": "1",
+    "EmployeeName": "Balarama",
+    "Role": "Admin",
+    "Salary":"40000",
+},
+{
+    "EmployeeID": "2",
+    "EmployeeName": "Sita",
+    "Role": "Testing",
+    "Salary":"25000",
+    
+},
+{
+    "EmployeeID": "3",
+    "EmployeeName": "Krishna",
+    "Role": "Developer",
+    "Salary":"60000",
+},
+{
+    "EmployeeID": "4",
+    "EmployeeName": "Ramu",
+    "Role": "Manager",
+    "Salary":"65000",
+}
+];
+module.exports=employees;
